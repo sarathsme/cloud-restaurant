4a329cd7f05123898a8925b88ae6b2b6819068d3
Sarath S Menon <sarathmenon1993@gmail.com>
2021-11-05 00:45:21 +0530

    feat: Set 'api' as root of Web API and add versioning
    - All APIs currently must start with /api/v1/

--- cloud-restaurant-api/CloudRestaurant.API/Controllers/v1/MenuController.cs
+++ cloud-restaurant-api/CloudRestaurant.API/Controllers/v1/MenuController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace CloudRestaurant.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class MenuController : ControllerBase
     {
         private readonly IMenuService _MenuService;
